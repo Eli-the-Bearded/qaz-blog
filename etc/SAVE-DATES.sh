6a2724be330614bb44dec7c3895f2c741d19ed4f
Eli the Bearded <github@eli.users.panix.com>
2020-03-17 00:20:42 -0400

    move SAVE-DATES.sh script

---
+++ etc/SAVE-DATES.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export SLS_DATEFMT=%Y%m%d%H%M.%S
+datadir=$PHTML/qz/data
+savefile=$PHTML/qz/etc/FIX-DATES
+# use sls to make shell commands to reset date/time on files
+slsoutstyle='touch -t %m %n'
+find "$datadir" -type f -exec sls -p "$slsoutstyle" {} + > "$savefile"
+
